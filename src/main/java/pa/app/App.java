9857036ae5425bb5b5e06963a200e6970ea7efb9
ricardo <oliveirabarbosagmail.com>
2018-09-12 16:43:26 +0100

    Update readme

---
+++ src/main/java/pa/app/App.java
@@ -0,0 +1,20 @@
+package pa.app;
+
+/**
+ * Maven JUnit 5 - Hello world!
+ * @author  Paulo Araújo
+ * @version 1.0
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Maven JUnit 5 - Hello World!" );
+    }
+	
+	// String concatenation method example
+	String concat(String s1, String s2) {
+		return s1+s2;
+	}
+}
